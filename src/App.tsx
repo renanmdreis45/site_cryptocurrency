32d0a034da9e46dbc30a1df96e171b4d3d7adcc9
renanmdreis <renanmdreis@alu.ufc.br>
2022-08-31 22:10:48 -0300

    Commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import light from './styles/themes/light';
+import dark from './styles/themes/dark';
+
+import GlobalStyle from './styles/global'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
+
+  const [theme, setTheme] = useState(light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light);
+  };
+
   return (
-    <div className="App">
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <GlobalStyle />
+        <Navbar toggleTheme={toggleTheme}/>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useContext } from 'react'
 import Logo from './logo.png'
-import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
-import './Navbar.css'
+import Switch from 'react-switch'
+import { ThemeContext } from 'styled-components'
+import { shade } from 'polished'
 
 
-interface ThemeProps {
-  themeStatus: boolean;
+interface Props {
+  toggleTheme(): void
 }
 
-const Navbar = ({themeStatus}:ThemeProps) =>{
+const Navbar: React.FC<Props> = ({toggleTheme}) => {
+  const { colors, title } = useContext(ThemeContext);
+
     return(
             <nav className="shadow-[0_3px_8px_rgba(0,0,0,0.24)] flex items-center">
               <div className="flex items-center">
@@ -17,20 +19,21 @@ const Navbar = ({themeStatus}:ThemeProps) =>{
                 <span className="text-lg text-black font-bold"> Crypto's </span>
               </div>
               <div>
-                 {themeStatus && (
-                    <FontAwesomeIcon
-                       className="cursor-pointer"
-                       icon={faSun}
-                     />
-                 )}
-                {!themeStatus && (
-                    <FontAwesomeIcon
-                       className="cursor-pointer"
-                       icon={faMoon}/>
-                )}
+                 <Switch
+                   onChange={toggleTheme}
+                   checked = {title === 'dark'}
+                   checkedIcon= {false}
+                   uncheckedIcon = {false}
+                   height={10}
+                   width = {40}
+                   handleDiameter={20}
+                   offColor={shade(0.15, colors.primary)}
+                   onColor={colors.secundary}
+
+                 />
              </div>            
             </nav>
     )
-}
+};
 
 export default Navbar;
